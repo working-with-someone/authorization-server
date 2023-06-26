2224b276023f0dd284478b02bb5ccc09db529ef5
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-06-26 16:21:52 +0900

    add middleware for any request to unkown endpoint

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,16 @@ app.use(express.static(`${process.cwd()}/wws-client/public/statics`));
 
 //import routers
 import { authRouter, homeRouter } from './router';
+import { wwsError } from './utils/wwsError';
 
 //use router
 app.use('/auth', authRouter);
 app.get('/', homeRouter);
+
+//response 404 for any request to unknown endpoint
+app.use('*', (req, res, next) => {
+  next(new wwsError(404, 'Can not found page'));
+});
 
 //error handler
 app.use(errorHandler);
