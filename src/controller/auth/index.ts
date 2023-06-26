f66bd7707ef0fc9244e29b5806d47cd4472ed430
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-06-26 16:08:55 +0900

    add error handling

--- src/controller/auth/index.ts
+++ src/controller/auth/index.ts
@@ -1,17 +1,29 @@
 import { Request, Response } from 'express';
 import OAuth from '../../lib/oauth';
+import { wwsError } from '../../utils/wwsError';
 
 export const renderSignin = (req: Request, res: Response) =>
   res.render('auth/signin');
 export const renderSignup = (req: Request, res: Response) =>
   res.render('auth/signup');
+
 export const redirectToAuth = (req: Request, res: Response) => {
-  return res.redirect(OAuth[req.params.provider].authURL);
+  try {
+    return res.redirect(OAuth[req.params.provider].authURL);
+  } catch (err) {
+    throw new wwsError(500, 'Problems moving to the consent screen');
+  }
 };
 
 export const codeCallback = async (req: Request, res: Response) => {
-  const authCode = req.query.code as string;
-  const accessToken = await OAuth[req.params.provider].getAccessToken(authCode);
+  try {
+    const authCode = req.query.code as string;
+    const accessToken = await OAuth[req.params.provider].getAccessToken(
+      authCode
+    );
 
-  return res.send(accessToken);
+    return res.send(accessToken);
+  } catch (err) {
+    throw new wwsError(500, 'Problems processing Oauth');
+  }
 };
