928c349cae9f497577dd1e8c3da1e251dc8f96b4
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-07-01 20:25:07 +0900

    fix : uncorrectly error handling

--- src/controller/auth/index.ts
+++ src/controller/auth/index.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import OAuth from '../../lib/api/oauth';
 import { wwsError } from '../../utils/wwsError';
 import HttpStatusCode from 'http-status-codes';
@@ -8,18 +8,28 @@ export const renderSignin = (req: Request, res: Response) =>
 export const renderSignup = (req: Request, res: Response) =>
   res.render('auth/signup');
 
-export const redirectToAuth = (req: Request, res: Response) => {
+export const redirectToAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     return res.redirect(OAuth[req.params.provider].authURL);
   } catch (err) {
-    throw new wwsError(
-      HttpStatusCode.INTERNAL_SERVER_ERROR,
-      'Problems moving to the consent screen'
+    next(
+      new wwsError(
+        HttpStatusCode.INTERNAL_SERVER_ERROR,
+        'Problems moving to the consent screen'
+      )
     );
   }
 };
 
-export const codeCallback = async (req: Request, res: Response) => {
+export const codeCallback = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const authCode = req.query.code as string;
     const accessToken = await OAuth[req.params.provider].getAccessToken(
@@ -28,9 +38,11 @@ export const codeCallback = async (req: Request, res: Response) => {
 
     return res.send(accessToken);
   } catch (err) {
-    throw new wwsError(
-      HttpStatusCode.INTERNAL_SERVER_ERROR,
-      'Problems processing Oauth'
+    next(
+      new wwsError(
+        HttpStatusCode.INTERNAL_SERVER_ERROR,
+        'Problems processing Oauth'
+      )
     );
   }
 };
