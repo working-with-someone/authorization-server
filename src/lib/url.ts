56c7126d6ff0d7830e981d978f4704affc672620
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-07-02 17:54:17 +0900

    update optional param

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -2,7 +2,11 @@ interface params {
   [key: string]: string | undefined;
 }
 
-function generateURL(url: string, params: params): string {
+function generateURL(url: string, params?: params): string {
+  if (!params) {
+    return url;
+  }
+
   return `${url}?${Object.keys(params)
     .map((key) => `${key}=${params[key]}`)
     .join('&')}`;
