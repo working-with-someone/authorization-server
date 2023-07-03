3fe9cac1e2fdf877b7b22fed627d2260dd719224
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-07-04 01:26:40 +0900

    refactor

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,6 +1,7 @@
 import { wwsError } from '../utils/wwsError';
 import { Request, Response, NextFunction } from 'express';
 import { sysErrorLogger } from '../logger/winston';
+import httpStatusCode from 'http-status-codes';
 
 const errorHandler = (
   err: wwsError,
@@ -8,11 +9,19 @@ const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  //should log errors that occur in other packages.
   if (err.originError) {
     const originError = err.originError;
 
     sysErrorLogger.error(originError.message, { stack: originError.stack });
   }
+
+  if (err.status == httpStatusCode.NOT_FOUND) {
+    // do not need to log wws not found error
+  } else {
+    // is optional to log other custom wws errors.
+  }
+
   return res.render('error/error', { err });
 };
 
