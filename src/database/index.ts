e5e66a946fd2e53f1131087a3576080e4035c8f1
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-07-12 16:30:59 +0900

    add logging for prisma queries

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,26 @@
 import { PrismaClient } from '@prisma/client';
+import { databaseLogger } from '../logger/winston';
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+});
 
-const prisma = new PrismaClient();
+prisma.$on('query', (e) => {
+  const message = `${e.query} / ${e.params} / ${e.duration}`;
+
+  databaseLogger.log('info', message);
+});
 
 export default prisma;
