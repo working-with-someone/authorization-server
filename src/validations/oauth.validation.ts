b3f8c11a46d7350c14fb099720fe39f9c9b2502e
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-08-14 23:11:09 +0900

    refactor extract providerValidation

--- src/validations/oauth.validation.ts
+++ src/validations/oauth.validation.ts
@@ -1,9 +1,13 @@
 import joi from 'joi';
 import OAuths from '../lib/api';
 
+const providerValidation: Record<string, any> = {
+  provider: joi.string().valid(...Object.keys(OAuths)),
+};
+
 const oauthRequestValidation: Record<string, any> = {
-  param: joi.object().keys({
-    provider: joi.string().valid(...Object.keys(OAuths)),
+  params: joi.object().keys({
+    ...providerValidation,
   }),
 };
 
