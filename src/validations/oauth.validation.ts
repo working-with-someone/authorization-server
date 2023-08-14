7459ced879184e416d4b7d4a77fe1b239b0c36b7
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-08-14 23:18:37 +0900

    add comment for oauth validation

--- src/validations/oauth.validation.ts
+++ src/validations/oauth.validation.ts
@@ -2,6 +2,7 @@ import joi from 'joi';
 import OAuths from '../lib/api';
 
 const providerValidation: Record<string, any> = {
+  //path parameter 'provider' required and must be one of registered OAuth Provider
   provider: joi.string().valid(...Object.keys(OAuths)),
 };
 
@@ -17,9 +18,11 @@ const authorizationCodeCallbackValidations: Record<string, any> = {
   }),
   query: joi
     .object()
+    //require authorization code in query parameters
     .keys({
       code: joi.string().required(),
     })
+    //allow other callback query parameters
     .unknown(),
 };
 
