9e9ee0a63b73332fc3e2a1af706b01af15b3e6ea
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-08-14 23:08:06 +0900

    add user model service layer skeleton

---
+++ src/services/user.service.ts
@@ -0,0 +1,17 @@
+import { Prisma } from '@prisma/client';
+
+export const createUser = async (body: Prisma.UserCreateInput) => {
+  return;
+};
+
+export const updateUser = async (body: Prisma.UserUpdateInput) => {
+  return;
+};
+
+export const deleteUser = async (body: Prisma.UserSelect) => {
+  return;
+};
+
+export const getUser = async (body: Prisma.UserSelect) => {
+  return;
+};
