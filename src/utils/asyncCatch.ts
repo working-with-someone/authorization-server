f9211f461649927284090a4e033397ee22770bc5
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-08-17 17:39:16 +0900

    response wwsError with expected error

--- src/utils/asyncCatch.ts
+++ src/utils/asyncCatch.ts
@@ -7,15 +7,22 @@ const asyncCatch =
   (req: Request, res: Response, next: NextFunction) => {
     //controller에서 error가 발생하지 않으면 무조건 resolve, express의 built-in async error handling이 동작하지 않음을 보장한다.
     //controller에서 error가 발생하면 custom error handler에게 err를 전달한다.
-    Promise.resolve(controller(req, res, next)).catch((err) =>
-      next(
-        new wwsError(
-          httpStatusCode.INTERNAL_SERVER_ERROR,
-          httpStatusCode.getStatusText(httpStatusCode.INTERNAL_SERVER_ERROR),
-          err
-        )
-      )
-    );
+    Promise.resolve(controller(req, res, next)).catch((err) => {
+      //expected된 error를 그대로 error handler에 보낸다.
+      if (err instanceof wwsError) {
+        next(err);
+      }
+      //unexpected error에 대해선 internal error를 생성해 error handler에 보낸다.
+      else {
+        next(
+          new wwsError(
+            httpStatusCode.INTERNAL_SERVER_ERROR,
+            httpStatusCode.getStatusText(httpStatusCode.INTERNAL_SERVER_ERROR),
+            err
+          )
+        );
+      }
+    });
   };
 
 export default asyncCatch;
