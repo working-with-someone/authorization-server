6480393906b0f999417affb5644f4d763b59b30c
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-08-17 16:06:20 +0900

    add getUser service logic

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,5 @@
 import { Prisma } from '@prisma/client';
+import prismaClient from '../database';
 
 export const createUser = async (body: Prisma.UserCreateInput) => {
   return;
@@ -12,6 +13,10 @@ export const deleteUser = async (body: Prisma.UserSelect) => {
   return;
 };
 
-export const getUser = async (body: Prisma.UserSelect) => {
-  return;
+export const getUser = async (body: Prisma.UserWhereInput) => {
+  const user = await prismaClient.user.findFirst({
+    where: body,
+  });
+
+  return user;
 };
