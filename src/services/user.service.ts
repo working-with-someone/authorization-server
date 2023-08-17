01d1931e862006e402d41dba1638491e539bc08d
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-08-17 21:11:11 +0900

    add createUser service logic

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,48 @@
 import { Prisma } from '@prisma/client';
 import prismaClient from '../database';
+import { wwsError } from '../utils/wwsError';
+import HttpStatusCode from 'http-status-codes';
+import bcrypt from 'bcrypt';
 
-export const createUser = async (data: Prisma.UserCreateInput) => {
-  return;
+interface UserCreateInput {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export const createUser = async (data: UserCreateInput) => {
+  const userAlreadyExist = await getUser({
+    local: {
+      email: data.email,
+    },
+  });
+
+  if (userAlreadyExist) {
+    throw new wwsError(
+      HttpStatusCode.CONFLICT,
+      'account already registered with email'
+    );
+  }
+
+  const salt = await bcrypt.genSalt(10);
+
+  const encrypted_password = await bcrypt.hash(data.password, salt);
+
+  const user = await prismaClient.user.create({
+    data: {
+      username: data.username,
+      pfp: '',
+      local: {
+        create: {
+          encrypted_password,
+          email: data.email,
+          email_verified: false,
+        },
+      },
+    },
+  });
+
+  return user;
 };
 
 export const updateUser = async (data: Prisma.UserUpdateInput) => {
