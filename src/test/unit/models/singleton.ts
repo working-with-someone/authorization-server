4f8cb81b2ea71c75118afcadd4a4b5b8b7172165
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-09-03 15:28:57 +0900

    refactor

--- src/test/unit/models/singleton.ts
+++ src/test/unit/models/singleton.ts
@@ -1,15 +1,10 @@
 import { PrismaClient } from '@prisma/client';
-import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
+import prisma from '../../../database';
 
-import prisma from './client';
-
-jest.mock('./client', () => ({
+jest.mock('../../../database/', () => ({
   __esModule: true,
   default: mockDeep<PrismaClient>(),
 }));
-
-beforeEach(() => {
-  mockReset(prismaMock);
-});
 
 export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
--- src/test/unit/models/user.test.ts
+++ src/test/unit/models/user.test.ts
@@ -1,18 +1,6 @@
-import prismaClient from './client';
 import { prismaMock } from './singleton';
+import { mockReset } from 'jest-mock-extended';
 
-test('expect create new user', async () => {
-  const newUser = {
-    id: 1,
-    username: 'seungho-hub',
-    pfp: 'https://www.google.com/favicon.ico',
-    created_at: new Date(),
-    updated_at: new Date(),
-  };
-
-  prismaMock.user.create.mockResolvedValue(newUser);
-
-  await expect(prismaClient.user.create({ data: newUser })).resolves.toEqual(
-    newUser
-  );
+beforeEach(() => {
+  mockReset(prismaMock);
 });
