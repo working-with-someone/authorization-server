65f584eebe961218e0aaf879a831b4ad88128877
seungho-hub <kmc54320@gmail.com>
2024-11-25 16:48:58 +0900

    add specific socket option to redis client

--- src/config/redisClient.config.ts
+++ src/config/redisClient.config.ts
@@ -5,6 +5,10 @@ const config: RedisClientOptions = {
   username: process.env.REDIS_USERNAME,
   password: process.env.REDIS_PASSWORD,
   database: parseInt(process.env.REDIS_DATABASE_NUMBER),
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT),
+  },
 };
 
 export default config;
